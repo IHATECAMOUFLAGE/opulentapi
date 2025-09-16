e506ed687b05de374f0ee1691e87b9480aa2f1d3
IHATECAMOUFLAGE <ihatecamouflage@hotmail.com>
2025-09-15 20:12:39 -0400

    Update sw.js

--- api/sw.js
+++ api/sw.js
@@ -1,13 +1,10 @@
-self.addEventListener('fetch', event=>{
-  const url = new URL(event.request.url);
-  if(url.origin !== self.origin){
-    event.respondWith(
-      fetch('/api/proxy?url='+encodeURIComponent(event.request.url),{
-        method:event.request.method,
-        headers:event.request.headers,
-        body:event.request.method==='POST'?event.request.clone().body:null,
-        credentials:'include'
-      })
-    );
-  }
+self.addEventListener('install',e=>{self.skipWaiting()});
+self.addEventListener('activate',e=>{self.clients.claim()});
+
+self.addEventListener('fetch',e=>{
+  const url = new URL(e.request.url);
+  if(url.origin === location.origin) return;
+  if(url.pathname.startsWith('/api/proxy')) return;
+  const proxied = '/api/proxy?url=' + encodeURIComponent(url.href);
+  e.respondWith(fetch(proxied));
 });

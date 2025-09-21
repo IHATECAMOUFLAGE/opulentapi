3a9d64ac9ff0585c17b33891a26952ab79e4e5d4
IHATECAMOUFLAGE <ihatecamouflage@hotmail.com>
2025-09-21 13:02:41 -0400

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
+    bare:'https://i-thought-i-could-trust-you-no-diddy-dog.satucat.uk/tspmo/' /* Yes, I stole this bare cry about it frfr */,
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
